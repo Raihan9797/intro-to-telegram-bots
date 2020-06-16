49b20a12309a3187970c8262ac68f50bee026910
Raihan9797 <raihanmuhd97@gmail.com>
2020-06-16 14:07:32 +0800

    added echoing of text messages

--- extensions_first_bot.py
+++ extensions_first_bot.py
@@ -26,4 +26,20 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+# updater.start_polling()
+
+
+
+################ ADDING THE MESSAGEHANDLER ##############
+# added a handler that listens to TEXT and echoes them
+def echo(update, context):
+    context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+
+# created a MessageHandler
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+# added it to the dispatcher
+dispatcher.add_handler(echo_handler)
+
 updater.start_polling()
